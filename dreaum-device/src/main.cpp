8bf9cb2f3f660e942790cf17f184f6ade6f4bfcd
hwun <hwun@hwunui-MacBookAir.local>
2025-06-27 13:23:04 +0900

    arduino setting

--- dreaum-device/src/main.cpp
+++ dreaum-device/src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+Servo myservo;
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("Starting...");
+  
+  myservo.attach(13);  // D13 = GPIO13
+  Serial.println("Servo attached");
 }
 
 void loop() {
-  Serial.println("Hello from DreaUm device!");
+  Serial.println("Moving to 0");
+  myservo.write(0);
+  delay(1000);
+  
+  Serial.println("Moving to 90");
+  myservo.write(90);
+  delay(1000);
+  
+  Serial.println("Moving to 180");
+  myservo.write(180);
   delay(1000);
 }
